0a2d5d9ce9ea7c88d30a6f8fde2ea568039eccfc
nhokcherry <baosuper456@gmail.com>
2023-04-18 20:22:37 +0700

    Change expired time token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -19,7 +19,7 @@ const jwt = require('jsonwebtoken');
 //Tao function de create JWT
 const generateToken = (id)=>{
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '3d'
+        expiresIn: '1d'
     })
 }
 
